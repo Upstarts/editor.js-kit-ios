3134c4168b788f604533703f6b16aa2d4ef97904
Ivan Glushko <i.glushko@upstarts.work>
2019-06-13 15:33:51 +0200

    Configurable View

--- EJ/EJHeaderBlockContent.swift
+++ EJ/EJHeaderBlockContent.swift
@@ -9,16 +9,16 @@
 import Foundation
 
 ///
-struct HeaderEJBlockContent: EJAbstractBlockContent {
-    private var items: [HeaderEJBlockContentItem] = []
+struct HeaderBlockContent: EJAbstractBlockContent {
+    private var items: [HeaderBlockContentItem] = []
     var numberOfItems: Int
     
     public static func decode(container: KeyedDecodingContainer<EJAbstractBlock.CodingKeys>) throws -> EJAbstractBlockContent {
-        let item = try container.decode(HeaderEJBlockContentItem.self, forKey: .content)
-        return HeaderEJBlockContent(items: [item])
+        let item = try container.decode(HeaderBlockContentItem.self, forKey: .data)
+        return HeaderBlockContent(items: [item])
     }
     
-    private init(items: [HeaderEJBlockContentItem]) {
+    private init(items: [HeaderBlockContentItem]) {
         self.items = items
         self.numberOfItems = items.count
     }
@@ -29,13 +29,12 @@ struct HeaderEJBlockContent: EJAbstractBlockContent {
 }
 
 ///
-struct HeaderEJBlockContentItem: EJAbstractBlockContentItem {
+class HeaderBlockContentItem: EJAbstractBlockContentItem {
     let text: String
     let level: Int
-}
-
-///
-public struct EJHeaderContentItem: EJAbstractBlockContentItem {
-    let text: String
-    let level: Int
+    
+    init(text: String, level: Int) {
+        self.text = text
+        self.level = level
+    }
 }
